9dc2b478efa45ff90945f62c12869ce56c78b9ab
kianoosh <58929848+yasin6606@users.noreply.github.com>
2023-01-30 21:06:26 +0330

    add hooks (useState, useEffect) for TopFooterMenu.tsx

--- src/Components/Footers/TopFooter/TopFooterMenu.tsx
+++ src/Components/Footers/TopFooter/TopFooterMenu.tsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {topFooterMenuItems} from "Data/TopFooterMenuData";
+import {TopFooterMenuItem} from "Assets/Types/GeneralTypes";
 
 const TopFooterMenu = (): JSX.Element => {
+
+    const [state, setState] = useState<TopFooterMenuItem[]>([]);
+
+    useEffect(() => {
+        setState(topFooterMenuItems);
+
+        return () => {
+            setState([]);
+        };
+    }, []);
+
     return <>
         <div className="text-white text-[13px] bg-dark-sub-nav flex flex-row justify-center py-2 md:py-12">
             {
-                topFooterMenuItems.map(item =>
+                state.map(item =>
                     <div key={item.topic} className="lg:px-10 flex flex-col">
-                            <p className="text-[16px] font-bold">{item.topic}</p>
-                            {
-                                item.subMenu.map(subItem =>
-                                    <div key={subItem.key} className="py-1">
-                                        <a href="#" className="text-[#DDD] text-[14px] hover:underline">{subItem.content}</a>
-                                    </div>
-                                )
-                            }
+                        <p className="text-[16px] font-bold">{item.topic}</p>
+                        {
+                            item.subMenu.map(subItem =>
+                                <div key={subItem.key} className="py-1">
+                                    <a href="#"
+                                       className="text-[#DDD] text-[14px] hover:underline">{subItem.content}</a>
+                                </div>
+                            )
+                        }
                     </div>
                 )
             }
